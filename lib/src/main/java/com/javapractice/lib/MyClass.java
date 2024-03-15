445df0aacd9ba23a45c8af24d7d86b52557b6c99
Yanderson Sena <contato@yandev.com.br>
2024-03-15 18:16:47 -0300

    Exercise Done

--- lib/src/main/java/com/javapractice/lib/MyClass.java
+++ lib/src/main/java/com/javapractice/lib/MyClass.java
@@ -1,11 +1,40 @@
 package com.javapractice.lib;
 
+import java.awt.SystemTray;
 import java.util.Scanner;
 
 public class MyClass {
     public static void main(String[] args){
 
         Scanner s1 = new Scanner(System.in);
+        String input_String = s1.nextLine();
+        cout(input_String);
+        System.out.println("Enter a text :");
+
+
+    }
+
+    public static void cout(String x){
+
+        char[] ch = x.toCharArray();
+        int letter = 0, space = 0, num = 0, others = 0;
+
+        for (int i = 0 ; i < x.length(); i++){
+            if(Character.isLetter(ch[i])){
+                letter++;
+            } else if(Character.isDigit(ch[i])){
+                num++;
+            } else if(Character.isSpaceChar(ch[i])){
+                space++;
+            } else {
+                others++;
+            }
+        }
+
+        System.out.println("Letters: " +letter);
+        System.out.println("Numbers: " +num);
+        System.out.println("Spaces: " +space);
+        System.out.println("Others: " +others);
 
     }
 }
