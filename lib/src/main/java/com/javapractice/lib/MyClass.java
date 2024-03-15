fa8c484f701ee794928d2f0edfd425c9769c0691
Yanderson Sena <contato@yandev.com.br>
2024-03-15 17:38:22 -0300

    Exercise Done - Yan Solution

--- lib/src/main/java/com/javapractice/lib/MyClass.java
+++ lib/src/main/java/com/javapractice/lib/MyClass.java
@@ -7,17 +7,11 @@ public class MyClass {
 
         Scanner s1 = new Scanner(System.in);
 
-        System.out.println("enter the first number");
+        System.out.println("Enter the decimal number :");
+        int number = s1.nextInt();
 
-        int n1 = s1.nextInt();
+        String binary = Integer.toBinaryString(number);
 
-        System.out.println("enter the second number");
-        int n2 = s1.nextInt();
-
-        float division = (float) n1 / n2;
-        float rest = n1 % n2;
-        System.out.println("n1 / n2 = " + division);
-        System.out.println("The remainder = " + rest);
-
+        System.out.println("This is the binary version = " + binary);
     }
 }
