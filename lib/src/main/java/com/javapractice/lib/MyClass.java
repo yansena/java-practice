c03f2ca6577c0b402161a129903d0420b234fd0c
Yanderson Sena <contato@yandev.com.br>
2024-03-15 17:50:12 -0300

    Exercise Done - Course Solution

--- lib/src/main/java/com/javapractice/lib/MyClass.java
+++ lib/src/main/java/com/javapractice/lib/MyClass.java
@@ -4,14 +4,27 @@ import java.util.Scanner;
 
 public class MyClass {
     public static void main(String[] args){
+        int dec_num, quot, i = 1, j;
+        int bin_num[] = new int[100];
 
         Scanner s1 = new Scanner(System.in);
 
         System.out.println("Enter the decimal number :");
-        int number = s1.nextInt();
 
-        String binary = Integer.toBinaryString(number);
+        dec_num = s1.nextInt();
 
-        System.out.println("This is the binary version = " + binary);
+        quot = dec_num;
+
+        while (quot != 0){
+            bin_num[i++] = quot % 2;
+            quot = quot/2;
+        }
+
+
+        System.out.println("This is the binary version = ");
+        for (j = i - 1; j > 0; j--){
+            System.out.println(bin_num[j]);
+        }
+
     }
 }
