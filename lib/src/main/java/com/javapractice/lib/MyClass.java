9792784837e249f2a71f6116108e36d003b1ed75
Yanderson Sena <contato@yandev.com.br>
2024-03-15 17:54:14 -0300

    Exercise Starter

--- lib/src/main/java/com/javapractice/lib/MyClass.java
+++ lib/src/main/java/com/javapractice/lib/MyClass.java
@@ -7,17 +7,5 @@ public class MyClass {
 
         Scanner s1 = new Scanner(System.in);
 
-        System.out.println("enter the first number");
-
-        int n1 = s1.nextInt();
-
-        System.out.println("enter the second number");
-        int n2 = s1.nextInt();
-
-        float division = (float) n1 / n2;
-        float rest = n1 % n2;
-        System.out.println("n1 / n2 = " + division);
-        System.out.println("The remainder = " + rest);
-
     }
 }
