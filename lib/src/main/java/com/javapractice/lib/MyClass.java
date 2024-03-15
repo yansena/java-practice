0273a7bf27c8ddd3e31af09c796608674c671aea
Yanderson Sena <contato@yandev.com.br>
2024-03-15 17:13:00 -0300

    Exercise Done

--- lib/src/main/java/com/javapractice/lib/MyClass.java
+++ lib/src/main/java/com/javapractice/lib/MyClass.java
@@ -1,5 +1,6 @@
 package com.javapractice.lib;
 
+import java.awt.SystemTray;
 import java.util.Scanner;
 
 public class MyClass {
@@ -7,17 +8,15 @@ public class MyClass {
 
         Scanner s1 = new Scanner(System.in);
 
-        System.out.println("enter the first number");
+        System.out.println("Type the radius :");
 
-        int n1 = s1.nextInt();
+        int radius= s1.nextInt();
 
-        System.out.println("enter the second number");
-        int n2 = s1.nextInt();
+        float perimeter = (float) ((float) 2 * Math.PI * radius);
+        float area = (float) Math.PI * radius*radius;
 
-        float division = (float) n1 / n2;
-        float rest = n1 % n2;
-        System.out.println("n1 / n2 = " + division);
-        System.out.println("The remainder = " + rest);
+        System.out.println("The perimeter is :" + perimeter);
+        System.out.println("The area is :" + area);
 
     }
 }
