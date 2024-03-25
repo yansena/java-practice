2c15315d885bad2435c40749f2670983b7969de7
Yanderson Sena <contato@yandev.com.br>
2024-03-25 14:36:06 -0300

    Done

--- lib/src/main/java/com/javapractice/lib/MyClass.java
+++ lib/src/main/java/com/javapractice/lib/MyClass.java
@@ -5,7 +5,20 @@ import java.util.Scanner;
 public class MyClass {
     public static void main(String[] args){
 
-        Scanner s1 = new Scanner(System.in);
+        int[] arrayOne = {1,3,-5,4};
+        int[] arrayTwo = {1,4,-5,-2};
+        String result = "";
+
+        for (int i=0;i < arrayOne.length ; i++ ){
+            int num1 = arrayOne[i];
+            int num2 = arrayTwo[i];
+
+            result += (num1 * num2)+ " ";
+        }
+
+        System.out.println("\nResult is: " + result);
+
+//        Scanner s1 = new Scanner(System.in);
 
     }
 }
